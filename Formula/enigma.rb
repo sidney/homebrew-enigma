2b1792b51ce157f4717f6bd7d744379980151931
sidney <sidney@sidney.com>
2021-08-19 12:48:52 +1200

    Updates that work with homebrew macos and linux for 1.30-beta

--- Formula/enigma.rb
+++ Formula/enigma.rb
@@ -1,34 +1,39 @@
 class Enigma < Formula
   desc "Puzzle game inspired by Oxyd and Rock'n'Roll"
   homepage "https://www.nongnu.org/enigma/"
-  url "https://github.com/Enigma-Game/Enigma/releases/download/1.30-beta/Enigma-macbuild-1.30-beta-src.tar.gz"
-  sha256 "54ff439d3c2092d39074136b536d1e1b7489f5c9353a2d55cfa56ae7f1013114"
+  url "https://github.com/Enigma-Game/Enigma/releases/download/1.30-beta/Enigma-1.30-beta-macbuild-src.tar.gz"
+  version "1.30"
+  sha256 "fe0a8436b862159fbd7d17bad1684f8f1df33bdff1b801bb3f904f3dbaab509e"
   license "GPL-2.0-or-later"
 
   livecheck do
     url :stable
-    regex(%r{url=.*?/enigma[._-]v?(\d+(?:\.\d+)+)\.t}i)
+    # This uses the git tag to get version numbers.
+    # Stable releases should be tagged like 1.21.1 or 1.30
+    # For now let it match on 1.30-beta so we can test with it
+    # and remove the (?:\D.*)? at the end when 1.30 is released
+    regex(/v?(\d+(?:\.\d+)+)(?:\D.*)?$/i)
   end
 
   bottle do
-    sha256 cellar: :any, catalina:    "38e4eb761c8c03ec2ff3221d576335d60c60ecb5f369e69098d34740118d48e4"
-    sha256 cellar: :any, mojave:      "8011aae1fa4e166dd9fb406844b1efcb246eb26ecc4e29c67dec71a3f8a7b231"
-    sha256 cellar: :any, high_sierra: "9eeb7a516f7188b38bc1a9e9ea2450db22391e65401d1377028881c11acbcc15"
-    sha256 cellar: :any, sierra:      "cdca7a198f3decfc3d387d590f84a7c3125adb06185469afa737eb5d61c150b3"
+    root_url "https://github.com/Enigma-Game/homebrew-enigma/releases/download/enigma-1.30"
+    sha256 catalina:     "aa9960a29a15c00bb81a8be1c77071252a2b62f565e2ecae711ace963d20234e"
+    sha256 big_sur:      "a6a021df393980f860b60a49132e6fb598ff11f33cf515ac2dbc06be80b86dcc"
+    sha256 x86_64_linux: "0b31cf13483d83f3e330d525b4e28c8daf81ee638739a4241daef3cf644e92b8"
   end
 
   head do
-    url "https://github.com/Enigma-Game/Enigma.git", branch: "macbuild"
+    url "https://github.com/Enigma-Game/Enigma.git", branch: "master"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "texi2html" => :build
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
   depends_on "imagemagick" => :build
-  depends_on "osxutils" => :build
   depends_on "pkg-config" => :build
   depends_on "enet"
   depends_on "freetype"
+  depends_on "gettext"
   depends_on "libpng"
   depends_on "sdl2"
   depends_on "sdl2_image"
@@ -36,10 +41,11 @@ class Enigma < Formula
   depends_on "sdl2_ttf"
   depends_on "xerces-c"
 
+  on_linux do
+    depends_on "gcc"
+  end
+
   def install
-    ENV.cxx11
-    ENV.append "CXXFLAGS", "-std=c++14"
-    ENV.append "LDFLAGS", "-liconv"
     system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--with-system-enet",
@@ -47,6 +53,13 @@ class Enigma < Formula
     system "make"
     system "make", "install"
     system "strip", bin/"enigma"
+    if build.head?
+      system "make", "dist"
+      mkdir_p prefix/"dist" if build.head?
+      dist_tar = Dir["enigma-*.tar.gz"]
+      odie "Unexpected number of artifacts!" if dist_tar.length != 1
+      (prefix/"dist").install dist_tar
+    end
     mkdir_p prefix/"etc"
     (prefix/"etc").install "etc/enigmabuilddmg"
     (prefix/"etc").install "etc/Info.plist"
