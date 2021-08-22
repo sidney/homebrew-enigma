b95498f8ad69a1fee205a68ea30438bd16113c02
sidney <sidney@sidney.com>
2021-08-22 14:52:57 +1200

    Add cask to install Enigma.app

---
+++ Casks/enigma.rb
@@ -0,0 +1,21 @@
+cask "enigma" do
+  version "1.30-beta"
+  sha256 "b99b128408d7a854c8e3e9ec8472b518c8941801484e078f54b81044f4798421"
+  url "https://github.com/Enigma-Game/Enigma/releases/download/#{version}/Enigma-#{version}.dmg",
+      verified: "github.com/Enigma-Game/"
+  name "Enigma"
+  desc "Puzzle game inspired by Oxyd and Rock'n'Roll"
+  homepage "https://www.nongnu.org/enigma/"
+
+  livecheck do
+    url :url
+    regex(/v?(\d+(?:\.\d+)+(?:\D.*)?)$/i)
+  end
+
+  suite "Enigma"
+
+  zap trash: [
+    "~/.enigmarc.xml",
+    "~/Library/Application Support/Enigma"
+  ]
+end
